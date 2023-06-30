36c8a4a35002b6f03708bd806a031da3d27e3f1a
Muanester <stevenmutisya999@gmail.com>
2023-06-30 14:05:23 +0300

    updated_todoList

---
+++ scripts/todoList.js
@@ -0,0 +1,47 @@
+const todoList = [{
+  name: "Make dinner",
+  dueDate: '2023-4-7',
+},
+{name: "Wash dishes",
+  dueDate: '2023-5-8'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoItem = todoList[i];
+    const { name, dueDate } = todoItem;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-btn">Delete</button>
+    `;
+    todoListHtml += html;
+
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+  }
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-todo-input');
+  const dateElement = document.querySelector('.js-date-input');
+
+  const dueDate = dateElement.value;
+  const name = inputElement.value;
+
+  todoList.push({name, dueDate});
+
+  inputElement.value = '';
+  dateElement.value = '';
+
+  renderTodoList();
+}
+
